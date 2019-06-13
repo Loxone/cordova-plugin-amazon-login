a3fda78b69953018ff9fae4e2aa26872df9cd446
Josh Noel <jnoel@leviton.com>
2019-06-13 15:23:48 -0500

    simplify 'after_prepare' script, add fs-extra dependency to stabilize fs operations

--- scripts/android/after_prepare.js
+++ scripts/android/after_prepare.js
@@ -1,13 +1,9 @@
 #!/usr/bin/env node
 'use strict';
-var fs = require('fs');
+var fs = require('fs-extra');
 
-var target = "debug";
-if (process.env.TARGET) {
-  target = process.env.TARGET;
-}
-
+var target = process.env.TARGET || 'debug';
+var apiKeyFilePath = 'platforms/android/app/src/main/assets/api_key.txt';
 var configobj = JSON.parse(fs.readFileSync('config/project.json', 'utf8'));
 
-var filename = "platforms/android/assets/api_key.txt";
-fs.writeFileSync(filename, configobj[target].AMAZON_API_KEY, 'utf8');
+fs.writeFileSync(apiKeyFilePath, configobj[target].AMAZON_API_KEY, 'utf8');
